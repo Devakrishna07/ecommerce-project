71410fe87520926f792a7c3f069d7c575b55f7a7
Devakrishna07 <135791446+Devakrishna07@users.noreply.github.com>
2025-03-04 19:39:02 +0530

    Delete frontend directory

--- frontend/src/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-{import('tailwindcss').Config}
-module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],  // Make sure Tailwind scans all JSX files
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-  
